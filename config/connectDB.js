32b489193f16179eb5521127476f413fb0a7f3dc
Ariful Hoque Al Amin <muhammadarif9416@yahoo.com>
2024-09-22 21:30:05 +0600

    remove: existing code

--- config/connectDB.js
+++
@@ -1,9 +0,0 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@firetools.k68hy.mongodb.net/?retryWrites=true&w=majority`;
-
-exports.client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
-});
